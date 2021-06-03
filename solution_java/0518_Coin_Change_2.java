1a7ab11235387fb5a71daab2446651c633a26756
mingchen47 <13725089621@163.com>
2021-06-03 10:01:17 +0800

    update

--- solution_java/0518_Coin_Change_2.java
+++ solution_java/0518_Coin_Change_2.java
@@ -1,44 +1,35 @@
-// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
-
-// Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
-
-// You may assume that you have an infinite number of each kind of coin.
-
-// The answer is guaranteed to fit into a signed 32-bit integer.
-
- 
-
-// Example 1:
-
-// Input: amount = 5, coins = [1,2,5]
-// Output: 4
-// Explanation: there are four ways to make up the amount:
-// 5=5
-// 5=2+2+1
-// 5=2+1+1+1
-// 5=1+1+1+1+1
-// Example 2:
-
-// Input: amount = 3, coins = [2]
-// Output: 0
-// Explanation: the amount of 3 cannot be made up just with coins of 2.
-// Example 3:
-
-// Input: amount = 10, coins = [10]
-// Output: 1
- 
-
-// Constraints:
-
-// 1 <= coins.length <= 300
-// 1 <= coins[i] <= 5000
-// All the values of coins are unique.
-// 0 <= amount <= 5000
-
-
 //思路：
 //这就是个经典的unbounded knackpack问题：
 //用a list of item有多少种填满size=amount背包的方式，可以重复选择同一个item
+
+// The difference is that if you update dp while:
+
+// increasing i then the previous partial result dp[i - coin] is the result that has considered coin already
+// decreasing i then the previous partial result dp[i - coin] is the result that has not considered coin yet
+
+// /** 
+//  * @return number of ways to make sum s using repeated coins
+//  */
+// public static int coinrep(int[] coins, int s) {
+//     int[] dp = new int[s + 1]; 
+//     dp[0] = 1;          
+//     for (int coin : coins)      
+//         for (int i = coin; i <= s; i++)         
+//             dp[i] += dp[i - coin];                                  
+//     return dp[s];
+// }                                       
+                                            
+// /**
+//  * @return number of ways to make sum s using non-repeated coins
+//  */
+// public static int coinnonrep(int[] coins, int s) {
+//     int[] dp = new int[s + 1];
+//     dp[0] = 1;  
+//     for (int coin : coins)
+//         for (int i = s; i >= coin; i--)
+//             dp[i] += dp[i - coin];              
+//     return dp[s];                                                   
+// } 
 
 class Solution {
   public int change(int amount, int[] coins) {
