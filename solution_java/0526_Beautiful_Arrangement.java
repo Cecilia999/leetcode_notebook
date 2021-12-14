09bd59da40aafb23dc0be33446d2ea4b79a4bcdb
mingchen47 <13725089621@163.com>
2021-12-14 15:52:04 +0800

    optimize 526. beatiful arrangement

--- solution_java/0526_Beautiful_Arrangement.java
+++ solution_java/0526_Beautiful_Arrangement.java
@@ -29,3 +29,32 @@ class Solution {
       }
   }
 }
+
+//优化写法，空间复杂度是O(n)，时间也更快
+class Solution {
+    private int count = 0;
+    
+    public int countArrangement(int n) {
+        boolean[] used = new boolean[n+1];
+        backtrack(1, used, n);
+        
+        return count;
+    }
+    
+    public void backtrack(int pos, boolean[] used, int n){
+        if(pos>n){
+            count++;
+            return;
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(used[i]) continue;
+
+            if(pos % i == 0 || i % pos == 0){
+                used[i] = true;
+                backtrack(pos+1, used, n);
+                used[i] = false;
+            }
+        }
+    }
+}
