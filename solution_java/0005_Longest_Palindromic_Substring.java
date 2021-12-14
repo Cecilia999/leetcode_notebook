b49f47b966174be628adb31c5baa7930546b8287
mingchen47 <13725089621@163.com>
2021-12-14 22:06:49 +0800

    update

--- solution_java/0005_Longest_Palindromic_Substring.java
+++ solution_java/0005_Longest_Palindromic_Substring.java
@@ -14,30 +14,35 @@
 // 中心拓展法 和647题一样
 
 class Solution {
-    int low = 0;
-    int maxLen = 0;
-    
+    public int max=0;
+    public int start=0;
     public String longestPalindrome(String s) {
         for(int i=0; i<s.length(); i++){
-            checkPalindrome(s, i, i);
-            if(i>0){
-                checkPalindrome(s, i-1, i);
-            }
+            isPalindromic(i, i, s);
+            if(i>0)
+                isPalindromic(i-1, i, s);
+            
         }
         
-        return s.substring(low, low + maxLen);
+        return s.substring(start, start+max);
     }
     
-    private void checkPalindrome(String s, int i, int j){
-        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j))
-        {
-            i--;
-            j++;
+    public void isPalindromic(int left, int right, String s){
+        while(left>=0 && right<s.length()){
+            if(s.charAt(left)!=s.charAt(right)){
+                break;
+            }
+            left--;
+            right++;
         }
-        //i和j多--和++了一次！！
-        if(maxLen<j-i-1){
-            low = i+1;
-            maxLen = j-i-1;
+        
+        left++;
+        right--;
+        
+        if(max < right-left+1){
+            max = right-left+1;
+            start = left;
         }
     }
 }
+
