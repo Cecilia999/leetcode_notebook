fbba36559cf695980f6e158c8a16e73b2b726327
mingchen47 <13725089621@163.com>
2021-12-19 16:31:52 +0800

    2刷 64 minpathsum

--- solution_java/0064_Minimum_Path_Sum.java
+++ solution_java/0064_Minimum_Path_Sum.java
@@ -11,27 +11,28 @@
 //解法1
 class Solution {
     public int minPathSum(int[][] grid) {
-        int row=grid.length, col=grid[0].length;
-        int[][] dp = new int[row][col];
+        int m=grid.length, n=grid[0].length;
+        int[][] dp = new int[m][n];
         
-        dp[0][0]=grid[0][0];
+        dp[0][0] = grid[0][0];
         
-        //要先initialize第一行和第一列 dp[0][j] & dp[i][0]
-        for(int i=1; i<row; i++){
-            dp[i][0]=grid[i][0]+dp[i-1][0];
-        }
-        for(int i=1; i<col; i++){
-            dp[0][i]=grid[0][i]+dp[0][i-1];
+        //rows
+        for(int i=1; i<n; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
         }
         
+        //cols
+        for(int i=1; i<m; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
         
-        for(int i=1; i<row; i++){
-            for(int j=1; j<col; j++){
-                dp[i][j]=Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j]; 
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
         
-        return dp[row-1][col-1];
+        return dp[m-1][n-1];
     }
 }
 
